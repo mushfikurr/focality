a741617926d4d4126bb15720d173b246641f9354
Mushfikur Rahman <mushfikur0@gmail.com>
2025-04-15 16:55:50 +0100

    Change font to space grotesk for readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import type { Metadata } from "next";
-import { IBM_Plex_Mono } from "next/font/google";
+import { IBM_Plex_Mono, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
-const ibmPlexMono = IBM_Plex_Mono({
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
-  style: ["normal", "italic"],
+const spaceGrotesk = Space_Grotesk({
+  weight: ["300", "400", "500", "600", "700"],
+  style: ["normal"],
   subsets: ["latin"],
   display: "swap",
 });
@@ -22,8 +22,8 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ConvexAuthNextjsServerProvider>
-      <html lang="en" className="dark">
-        <body className={`${ibmPlexMono.className} relative antialiased`}>
+      <html lang="en" className="light">
+        <body className={`${spaceGrotesk.className} relative antialiased`}>
           <ConvexClientProvider>{children}</ConvexClientProvider>
           <Toaster />
         </body>
