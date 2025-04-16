e11073837b2b16b9fca5d2491ce094212434a63e
Mushfikur Rahman <mushfikur0@gmail.com>
2025-04-16 16:12:52 +0100

    Add user-avatar component with popover

---
+++ components/common/user-avatar.tsx
@@ -0,0 +1,150 @@
+import { Doc } from "@/convex/_generated/dataModel";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Badge } from "../ui/badge";
+import {
+  Ban,
+  Clock,
+  Crown,
+  MessageCircle,
+  Star,
+  Trophy,
+  UserPlus,
+  Volume2,
+} from "lucide-react";
+import { Separator } from "../ui/separator";
+import { Button } from "../ui/button";
+
+type User = Doc<"users">;
+
+export default function UserAvatar({ user }: { user: User }) {
+  return (
+    <Popover key={user._id}>
+      <PopoverTrigger>
+        <Avatar
+          key={user._id}
+          className="h-7 w-7 rounded-none border shadow-sm"
+        >
+          <AvatarImage src={user.image} />
+          <AvatarFallback className="h-full w-full rounded-none">
+            {user.name?.[0] ?? "A"}
+          </AvatarFallback>
+        </Avatar>
+      </PopoverTrigger>
+      <PopoverContent className="max-w-[500px]">
+        <UserCard user={user} />
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+function UserCard({ user }: { user: User }) {
+  return (
+    <div className="flex flex-col">
+      {/* Header with background */}
+      <div className="rounded-t-lg p-0">
+        <div className="flex gap-2.5">
+          <Avatar className="h-14 w-14 rounded-none border">
+            <AvatarImage src={user.image} alt={user.name} />
+            <AvatarFallback>
+              {user.name?.substring(0, 2).toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+
+          <div className="flex-1">
+            <div className="flex items-center justify-between">
+              <h3 className="text-lg font-semibold">{user.name}</h3>
+              {/* {user.isHost && ( */}
+
+              {/* )} */}
+            </div>
+            <div className="mt-1 flex items-center gap-2">
+              <span className={`bg-primary h-2 w-2`}></span>
+              <span className="text-muted-foreground text-sm">Working</span>
+            </div>
+            {/* {user.role && (
+                <div className="mt-1">
+                  <Badge variant="secondary" className="text-xs">
+                    {user.role}
+                  </Badge>
+                </div>
+              )} */}
+          </div>
+          <Badge
+            variant="outline"
+            className="flex h-5 items-center gap-1 border-amber-500/20 bg-amber-500/10 px-1.5 text-amber-500"
+          >
+            <Crown className="h-3 w-3" />
+            <span className="text-xs">Host</span>
+          </Badge>
+        </div>
+      </div>
+
+      {/* User stats */}
+      <div className="mx-auto grid grid-cols-2 gap-x-8 gap-y-3 py-4">
+        <div className="col-span-1 flex items-center gap-2">
+          <div className="bg-primary/10 flex h-8 w-8 items-center justify-center">
+            <Clock className="icon-primary h-4 w-4" />
+          </div>
+          <div>
+            <p className="text-muted-foreground text-xs">Focus</p>
+            <p className="font-medium">{"0h 0m"}</p>
+          </div>
+        </div>
+        <div className="col-span-1 flex items-center gap-2">
+          <div className="flex h-8 w-8 items-center justify-center bg-amber-500/10">
+            <Trophy className="h-4 w-4 text-amber-500" />
+          </div>
+          <div>
+            <p className="text-muted-foreground text-xs">Streak</p>
+            <p className="font-medium">{0} days</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="flex h-8 w-8 items-center justify-center bg-purple-500/10">
+            <Star className="h-4 w-4 text-purple-500" />
+          </div>
+          <div>
+            <p className="text-muted-foreground text-xs">Level</p>
+            <p className="font-medium">{1}</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="flex h-8 w-8 items-center justify-center bg-blue-500/10">
+            <Trophy className="h-4 w-4 text-blue-500" />
+          </div>
+          <div>
+            <p className="text-muted-foreground text-xs">XP</p>
+            <p className="font-medium">{0} XP</p>
+          </div>
+        </div>
+      </div>
+
+      <Separator />
+
+      {/* Action buttons */}
+      <div className="grid grid-cols-2 gap-2 pt-3">
+        <Button variant="secondary" size="sm" className="w-full">
+          <MessageCircle className="h-4 w-4" />
+          Message
+        </Button>
+        <Button variant="secondary" size="sm" className="w-full">
+          <Volume2 className="h-4 w-4" />
+          Call
+        </Button>
+        <Button variant="outline" size="sm" className="w-full">
+          <UserPlus className="h-4 w-4" />
+          Add Friend
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          className="text-destructive hover:text-destructive w-full"
+        >
+          <Ban className="h-4 w-4" />
+          Block
+        </Button>
+      </div>
+    </div>
+  );
+}
