b2e8783c2a75739b26b0d9be118b80aaf6ba37b0
Mushfikur Rahman <mushfikur0@gmail.com>
2025-09-25 00:12:46 +0100

    add grid to loading state

--- app/(main)/explore/loading.tsx
+++ app/(main)/explore/loading.tsx
@@ -1,5 +1,9 @@
 import SessionListSkeleton from "@/components/explore/session-list-skeleton";
 
 export default function ExploreLoading() {
-  return <SessionListSkeleton />;
+  return (
+    <div className="grid grid-cols-1 gap-4 pb-4 md:grid-cols-3 lg:grid-cols-4">
+      <SessionListSkeleton />
+    </div>
+  );
 }
